08acf3701357cc12304bdbf521f213eec1e1684c
dawn0829 <w7823125@gmail.com>
2022-10-08 05:24:27 +0800

    AddMSG

--- dutyShell.sh
+++ dutyShell.sh
@@ -1,1 +1,6 @@
 ##This is Add_dutyShell
+MSG="jiaren SB"
+
+curl --location --request POST 'https://notify-api.line.me/api/notify' \
+--header 'Authorization: Bearer qm0oix3SyLMnBK6AuitdcvkZI0Wvf7T7qDuQyHgnjWu' \
+--form "message=${MSG}"
