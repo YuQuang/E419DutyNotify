95cdf10c87820fa10fb50a01ff1a3b282d93f952
dawn0829 <w7823125@gmail.com>
2022-10-08 05:31:38 +0800

    Add Hello (Issue: #4)

--- dutyShell.sh
+++ dutyShell.sh
@@ -4,3 +4,5 @@ MSG="jiaren SB"
 curl --location --request POST 'https://notify-api.line.me/api/notify' \
 --header 'Authorization: Bearer qm0oix3SyLMnBK6AuitdcvkZI0Wvf7T7qDuQyHgnjWu' \
 --form "message=${MSG}"
+
+echo Hello
