77e53b71772e981de8836fe6748b4be24107d607
RoyXu <bbb233456@gmail.com>
2022-10-08 06:54:57 +0800

    add unix timestamp calculator

--- dutyShell.sh
+++ dutyShell.sh
@@ -1,8 +1,19 @@
-##This is Add_dutyShell
-MSG="jiaren SB"
+ARR=$( \
+while IFS=, read line
+do 
+    echo $line
+done < shiftTable.csv )
+
+echo "ARR=${ARR}"
+
+IFS=',' read -ra DATE <<< "$ARR"
+for i in "${DATE[@]}"; do
+  echo "Line ${i}"
+done
+
+DAY=$(expr $(expr $(date '+%s') + 28800) / 86400)
+ORDER=$(expr $DAY % ${#DATE[@]})
 
 curl --location --request POST 'https://notify-api.line.me/api/notify' \
 --header 'Authorization: Bearer qm0oix3SyLMnBK6AuitdcvkZI0Wvf7T7qDuQyHgnjWu' \
---form "message=${MSG}"
-
-echo Hello
+--form "message=${DATE[$ORDER]}"
